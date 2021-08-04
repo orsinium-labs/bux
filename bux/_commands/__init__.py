070ed8aa09e1c30f280454889bfa43c0572825d7
gram <mail@orsinium.dev>
2021-08-04 13:48:13 +0200

    gainers and losers CLI

--- bux/_commands/__init__.py
+++ bux/_commands/__init__.py
@@ -1,8 +1,10 @@
 from ._base import Command, commands
+from ._gainers import Gainers
 from ._get_token import GetToken
 from ._categories import Categories
 from ._countries import Countries
 from ._inbox import Inbox
+from ._losers import Losers
 from ._me import Me
 from ._new_stocks import NewStocks
 from ._search import Search
@@ -12,11 +14,13 @@ from ._tags import Tags
 
 __all__ = [
     'commands',
-    'Command',
-    'GetToken',
     'Categories',
+    'Command',
     'Countries',
+    'Gainers',
+    'GetToken',
     'Inbox',
+    'Losers',
     'Me',
     'NewStocks',
     'Search',
---
+++ bux/_commands/_losers.py
@@ -0,0 +1,20 @@
+from argparse import ArgumentParser
+from .._user import UserAPI
+from ._base import Command, register
+
+
+@register
+class Losers(Command):
+    name = 'losers'
+
+    @staticmethod
+    def init_parser(parser: ArgumentParser) -> None:
+        parser.add_argument('--token', required=True)
+
+    def run(self) -> int:
+        api = UserAPI(token=self.args.token)
+        movers = api.securities().movers().requests()
+        for stock in movers.losers:
+            gain = (stock.bid.amount / stock.closing_bid.amount - 1) * 100
+            print(f'{stock.id} {stock.name:25} {gain:+.02f}%')
+        return 0
